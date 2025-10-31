0a35f110a3db450e60ce5c000bd574f819a921f5
Denis Kochetkov <ikotyaxx@yadnex.ru>
2025-10-31 10:11:12 +0100

    persist

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import travelReducer from '../features/travel/travelSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-export const store = configureStore({
-    reducer: {
-        travel: travelReducer,
-    },
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['travel'],
+};
+
+const rootReducer = combineReducers({
+    travel: travelReducer,
 });
 
-// типы для хуков
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
+});
+
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { Provider } from 'react-redux';
-import { store } from './app/store.ts';
+import { persistor, store } from './app/store.ts';
+import { PersistGate } from 'redux-persist/integration/react';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <Provider store={store}>
-            <App />
+            <PersistGate persistor={persistor} loading={null}>
+                <App />
+            </PersistGate>
         </Provider>
     </StrictMode>,
 );
