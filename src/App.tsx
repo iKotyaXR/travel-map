90072cb5fa135a2eae3db17934bed49e421e1126
Денис Кочетков <you@example.com>
2025-10-31 11:48:53 +0100

    Switch to hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
 import Container from './Components/Container/Container.tsx';
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { Route, Routes, Navigate, HashRouter } from 'react-router-dom';
 import Countries from './Components/pages/Countries/Contries.tsx';
 import Map from './Components/pages/Map/Map.tsx';
 import Stats from './Components/pages/Stats/Stats.tsx';
@@ -8,14 +8,14 @@ import Stats from './Components/pages/Stats/Stats.tsx';
 function App() {
     return (
         <Container>
-            <BrowserRouter>
+            <HashRouter>
                 <Routes>
                     <Route path="/" element={<Countries />} />
                     <Route path="/map" element={<Map />} />
                     <Route path="/stats" element={<Stats />} />
                     <Route path="*" element={<Navigate to="/" replace />} />
                 </Routes>
-            </BrowserRouter>
+            </HashRouter>
         </Container>
     );
 }
