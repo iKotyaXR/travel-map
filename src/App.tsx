d151ed014179f03262b852046d7b8ae51cfd53ca
Denis Kochetkov <ikotyaxx@yadnex.ru>
2025-10-31 10:50:42 +0100

    hash

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
 import Container from './Components/Container/Container.tsx';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Countries from './Components/pages/Countries/Contries.tsx';
 import Map from './Components/pages/Map/Map.tsx';
 import Stats from './Components/pages/Stats/Stats.tsx';
@@ -8,13 +8,13 @@ import Stats from './Components/pages/Stats/Stats.tsx';
 function App() {
     return (
         <Container>
-            <BrowserRouter>
+            <HashRouter>
                 <Routes>
                     <Route path="/" element={<Countries />} />
                     <Route path="/map" element={<Map />} />
                     <Route path="/stats" element={<Stats />} />
                 </Routes>
-            </BrowserRouter>
+            </HashRouter>
         </Container>
     );
 }
